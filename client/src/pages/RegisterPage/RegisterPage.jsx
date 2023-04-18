44ee84bfa23326c13a7ffaa621bde0a89af5d76c
Aleksey <aleksey.levada@gmail.com>
2023-04-18 18:42:57 +0300

    some updates

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,50 +1,55 @@
-import React, {useState} from 'react';
-import {useDispatch} from "react-redux";
-import {registerUser} from "../../store/thunks/registerUser";
-import {StyledRegisterPage} from './styles/styledRegisterPage';
-import {StyledAuthForm} from "./styles/styledAuthForm";
-import {StyledButtonsBlock} from "./styles/styledButtonsBlock";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { registerUser } from "../../store/thunks/registerUser";
+import { StyledRegisterPage } from "./styles/styledRegisterPage";
+import { StyledAuthForm } from "./styles/styledAuthForm";
+import { StyledButtonsBlock } from "./styles/styledButtonsBlock";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 export const RegisterPage = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const dispatch = useDispatch()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-    const getRegisterInfo = () => {
-        try {
-            dispatch(registerUser({username, password}))
-            setUsername('')
-            setPassword('')
-        }
-        catch (err){
-            console.log(err)
-        }
+  const getRegisterInfo = () => {
+    try {
+      dispatch(registerUser({ username, password }));
+      setUsername("");
+      setPassword("");
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return(
-        <StyledRegisterPage>
-            <StyledAuthForm onSubmit={(e) => e.preventDefault()}>
-                <h2>
-                    Регистрация
-                </h2>
-                <label>
-                    Username:
-                    <input placeholder={'Username'} name={'username'} type={'text'} onChange={(e) => setUsername(e.target.value)}/>
-                </label>
-                <label>
-                    Password:
-                    <input placeholder={'Password'} name={'password'} type={'password'} onChange={(e) => setPassword(e.target.value)}/>
-                </label>
-                <StyledButtonsBlock>
-                    <button type={'submit'} onClick={() => getRegisterInfo()}>
-                        Подтвердить
-                    </button>
-                    <Link to='/login'>
-                        Есть аккаунт?
-                    </Link>
-                </StyledButtonsBlock>
-            </StyledAuthForm>
-        </StyledRegisterPage>
-    )
-}
+  return (
+    <StyledRegisterPage>
+      <StyledAuthForm onClick={(e) => e.preventDefault()}>
+        <h2>Регистрация</h2>
+        <label>
+          Username:
+          <input
+            placeholder={"Username"}
+            name={"username"}
+            type={"text"}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            placeholder={"Password"}
+            name={"password"}
+            type={"password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <StyledButtonsBlock>
+          <button type={"submit"} onClick={() => getRegisterInfo()}>
+            Подтвердить
+          </button>
+          <Link to="/login">Есть аккаунт?</Link>
+        </StyledButtonsBlock>
+      </StyledAuthForm>
+    </StyledRegisterPage>
+  );
+};
